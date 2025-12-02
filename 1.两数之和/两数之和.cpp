41b9d96078fa899088438b2e2048f56990eb25e6
mumuXmumu <7627733+mumuxmumu@user.noreply.gitee.com>
2025-12-02 10:59:56 +0800

    两数相加

---
+++ 1.两数之和/两数之和.cpp
@@ -0,0 +1,61 @@
+/*
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+
+你可以按任意顺序返回答案。
+
+
+
+示例 1：
+
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+示例 2：
+
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+示例 3：
+
+输入：nums = [3,3], target = 6
+输出：[0,1]
+
+*/
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+
+
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> output(2);
+            for (int i = 0; i < nums.size() - 1; i++)
+            {
+                for (int j = i+1; j < nums.size(); j++)
+                {
+                    if (nums[i] + nums[j] ==  target)
+                    {
+                        return output = {i,j };
+                    }
+                }
+
+            }
+            return output;
+    }
+
+
+};
+
+
+int main()
+{
+    Solution solution;
+    vector<int> nums = { 8,2,3, 7, 11, 15 };
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+    cout << "[" << result[0] << "," << result[1] << "]" << endl;
+}
---
+++ leetcode/2.两数相加/两数相加.cpp
@@ -0,0 +1,111 @@
+/*
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#include <vector>
+#include <iostream>
+using namespace std;
+ struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0); // 虚拟头节点，简化边界处理
+        ListNode* curr = &dummy;
+        int carry = 0;
+        while (l1 != nullptr || l2 != nullptr || carry != 0)
+        {
+            int val1 = (l1 != nullptr) ? l1->val : 0;
+            int val2 = (l2 != nullptr) ? l2->val : 0;
+
+            int total = val1 + val2 + carry;
+
+            carry = total / 10;
+            int digit = total % 10;
+
+            curr->next = new ListNode(digit);
+            curr = curr->next;
+
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
+
+        }
+        return dummy.next;
+    }
+};
+
+//错误，仅返回了list的最后的元素，而不是返回整个链表
+ListNode* createList_error(const vector<int>& nums)
+{
+    ListNode* output = new ListNode(nums[0]);
+    for (int i = 1; i < nums.size(); i++)
+    {
+        output->next = new ListNode(nums[i]);
+        output = output->next;
+    }
+    return output;
+}
+
+
+ListNode* createList(const std::vector<int>& nums) {
+    if (nums.empty()) return nullptr;
+    ListNode* head = new ListNode(nums[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < nums.size(); ++i) {
+        curr->next = new ListNode(nums[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+void printList(ListNode* head)
+{
+
+    cout << "[";
+
+    while (head != nullptr)
+    {
+        cout << head->val ;
+        if (head->next != nullptr) std::cout << ",";
+
+        head = head->next;
+    }
+
+    cout << "]";
+}
+
+int main()
+{
+    Solution listNode;
+    ListNode* l1 = createList({2,3,4});
+    ListNode* l2 = createList({ 5,6,4 });
+    printList(l1);
+
+    ListNode* result =  listNode.addTwoNumbers(l1, l2);
+    printList(result);
+}
