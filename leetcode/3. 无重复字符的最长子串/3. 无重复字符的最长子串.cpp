ddc13bbee50186db2bdce77979cb5e729faa89dc
mumuXmumu <7627733+mumuxmumu@user.noreply.gitee.com>
2025-12-03 14:34:10 +0800

    补充扩展

--- leetcode/3. 无重复字符的最长子串/3. 无重复字符的最长子串.cpp
+++ leetcode/3. 无重复字符的最长子串/3. 无重复字符的最长子串.cpp
@@ -41,7 +41,88 @@ public:
     }
     //暴力法：
 
+    //返回某一最长无重复字符子串，并通过引用输出长度
+    string findLongestSubstring(const string& s, int& maxLength)
+    {
+        unordered_set<char> seen;
+        int left = 0, right = 0;
+        maxLength = 0;
+        int firstIndex = 0;//要返回的字串起始位置
+        string result;
+        while (right < s.size())
+        {
+            if (seen.count(s[right])==0)
+            {
+                seen.insert(s[right]);
+                int currLength = right - left + 1;
+                if(currLength> maxLength)
+                {
+                    maxLength = currLength;
+                    firstIndex = left;
+                }
+                ++right;
+                
+            }
+            else
+            {
 
+                seen.erase(s[left]);
+                ++left;
+            }
+        }
+        for (int i = firstIndex; i < firstIndex+maxLength; i++)
+        {
+            result.push_back(s[i]);
+        }
+
+        return result;
+
+    }
+
+    //返回所有最长无重复字符子串，并通过引用输出长度
+    vector<string> findAllLongestSubstring(const string& s, int& maxLength)
+    {
+        unordered_set<char> seen;
+        int left = 0, right = 0;
+        maxLength = 0;
+        int firstIndex = 0;//要返回的字串起始位置
+        vector<string> result;
+        while (right < s.size())
+        {
+            if (seen.count(s[right]) == 0)
+            {
+                seen.insert(s[right]);
+                int currLength = right - left + 1;
+                if (currLength > maxLength)
+                {
+                    maxLength = currLength;
+                    firstIndex = left;
+
+                    result.clear();
+                    result.push_back(s.substr(left, currLength));
+                }
+                else  if (currLength ==  maxLength)
+                {
+                    firstIndex = left;
+                    result.push_back(s.substr(left, currLength));
+                }
+                ++right;
+
+            }
+            else
+            {
+
+                seen.erase(s[left]);
+                ++left;
+            }
+        }
+        
+
+        return result;
+
+    }
+
+    //返回所有无重复字符子串，
 
 };
 
@@ -52,10 +133,22 @@ int main()
     string a = "";
     string b = "abcabcbb";
     string c = "pwwkew";
-    cout << solute.lengthOfLongestSubstring(s) << endl;
+    string d = "abcbed";
+    string e = "abcabc";
+   /* cout << solute.lengthOfLongestSubstring(s) << endl;
     cout << solute.lengthOfLongestSubstring(a) << endl;
     cout << solute.lengthOfLongestSubstring(b) << endl;
-    cout << solute.lengthOfLongestSubstring(c) << endl;
+    cout << solute.lengthOfLongestSubstring(c) << endl;*/
+    cout << "changdu "<< solute.lengthOfLongestSubstring(d) << endl;
 
+    int maxLength;
+    cout << solute.findLongestSubstring(d, maxLength) << endl;
+    cout << "长度为："<< maxLength << endl;
+
+    vector<string> allLongestString = solute.findAllLongestSubstring(e, maxLength);
+    for (string str : allLongestString)
+    {
+        cout << str << ",";
+    }
     return 0;
 }
